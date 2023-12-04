3410395b43c33e77c2ae32956a3ea8dfa5e7cffd
SHOVONIL ROY <124487582+shovonil@users.noreply.github.com>
2023-12-04 19:40:24 +0530

    Add files via upload
    add first assignment

---
+++ Replace pi (recursive).java
@@ -0,0 +1,23 @@
+import java.io.CharArrayReader;
+
+public class solution{
+
+
+	public static String replace(String input){
+
+		if(input.length()<=1){
+			return input;
+		}
+
+		if(input.charAt(0)=='p' && input.charAt(1)=='i'){
+
+			String SmallOutput=replace(input.substring(2));
+			return 3.14+SmallOutput;
+		}else{
+				String SmallOutput=replace(input.substring(1));
+				return input.charAt(0)+SmallOutput;
+		}
+	}
+
+	
+}
